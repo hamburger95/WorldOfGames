0b43e335bb4907014583afbf80b1a159d17d5c79
Ido Hamburger <ihamburger@outbrain.com>
2020-07-09 00:39:02 +0300

    clean e2e.py

--- e2e.py
+++ e2e.py
@@ -4,7 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 site_url = "http://127.0.0.1:5000/"
-#site_url = "http://127.0.0.1:8777/"
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(3)
@@ -13,15 +12,12 @@ driver.implicitly_wait(3)
 def test_scores_service(app_url):
     try:
         driver.get(site_url)
-        delay = 3  # seconds
         s_check = int(driver.find_element_by_id("score").text)
 
 
     except type:
         print("test failed")
     finally:
-        # browser switch
-        #time.sleep(10)
         driver.close()
 
     return (s_check >= 1 and s_check <= 1000)
