afcdde9abe3e48cce5b0fa95c5fb25065d6ca169
Ido Hamburger <ihamburger@outbrain.com>
2020-07-12 09:26:41 +0300

    working e2e.py clean

--- e2e.py
+++ e2e.py
@@ -1,42 +1,3 @@
-# # coding=utf-8
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-#
-# site_url = "http://127.0.0.1:5000/"
-#
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver.implicitly_wait(3)
-#
-#
-# def test_scores_service(app_url):
-#     try:
-#         driver.get(site_url)
-#         s_check = int(driver.find_element_by_id("score").text)
-#
-#
-#     except type:
-#         print("test failed")
-#     finally:
-#         driver.close()
-#
-#     return (s_check >= 1 and s_check <= 1000)
-#
-#
-# def main_function():
-#     flag = test_scores_service(site_url)
-#     print(flag)
-#     assert flag
-#
-#
-# main_function()
-
-
-######################################## NEW branch ###################################################################
-"""
-A simple selenium test example written by python
-"""
-
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -58,7 +19,7 @@ class TestTemplate(unittest.TestCase):
         self.driver.quit()
 
     def test_case_1(self):
-        """Find and click top-right button"""
+        """Find and get score"""
         try:
             site_url = "http://127.0.0.1:5000/"
             self.driver.get(site_url)
@@ -66,15 +27,8 @@ class TestTemplate(unittest.TestCase):
             print(s_check)
         except NoSuchElementException as ex:
             self.fail(ex.msg)
+        assert (s_check >= 1 and s_check <= 1000)
 
-    def test_case_2(self):
-        """Find and click Learn more button"""
-        try:
-            self.driver.get('https://www.oursky.com/')
-            el = self.driver.find_element_by_xpath(".//*[@id='tag-line-wrap']/span/a")
-            el.click()
-        except NoSuchElementException as ex:
-            self.fail(ex.msg)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
